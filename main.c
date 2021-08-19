b688281fb69909f39540e0405bc78e90c0e7da14
LaloPaniagua <65317268+LaloPaniagua@users.noreply.github.com>
2021-08-19 13:51:56 -0500

    Update main.c

--- main.c
+++ main.c
@@ -5,8 +5,12 @@ int main(){
     char file[80],*token,contenido_archivo[80];
     int i,opcion_01;
     double value,valores_leidos[80];
+    
+    //Ejecución de la función para generar una expresión postfija
     COLA *expresion_postfija=postfija();          
     
+    
+    //Menú para elegir si guardar o no la expresión postfija en un documento de texto
     printf("Desea guardar la expresion postfija en un documento de texto?\n1)Si\n2)No\n");
     scanf("%d",&opcion_01);
     
@@ -16,6 +20,7 @@ int main(){
         getchar();
     }
 
+    //Guardado de la expresión en el archivo postfijas.txt
     if(opcion_01==1){
         archivo=fopen("postfijas.txt","w+t");
         for(NODO* temporal=expresion_postfija->head;temporal!=NULL;temporal=temporal->sig){
@@ -25,6 +30,8 @@ int main(){
         fclose(archivo);
         printf("La expresion ha sido guardada en el archivo postfijas.txt\n");
     }
+    
+    //Se le pregunta al usuario si desea leer los valores a evaluar de un archivo
     printf("Desea leer los valores a evaluar desde un archivo?\n");
     printf("1)Si (Considera que los numeros en el archivo deben estar separados por una coma)\n2)No, prefiero ingresarlos desde la consola\n");
     scanf("%d",&opcion_01);
@@ -35,6 +42,8 @@ int main(){
         getchar();
     }
     
+    
+    //El usuario ingresa los valores a evaluar
     if(opcion_01==2){
         for(NODO* temporal = expresion_postfija->head;temporal!=NULL;temporal=temporal->sig){
             if(temporal->operador==false) {
@@ -45,9 +54,11 @@ int main(){
         }
     }
 
+    //Se le pide al usuario ingresar el nombre del archivo a leer
     else if(opcion_01==1){
         printf("Ingresa el nombre del archivo a leer:\n");
         scanf("%s",&file);
+        //Apertura del archivo a leer
         archivo=fopen(file,"r+t");
         while (archivo==NULL)
         {
@@ -57,15 +68,17 @@ int main(){
             archivo=fopen(file,"r+t");
         }
         i=0;
+        //Lectura del contenido del archivo
         fscanf(archivo,"%s",&contenido_archivo);
         fclose(archivo);
+        //Separación de los números contenidos en la cadena leída
         token=strtok(contenido_archivo,",");
         printf("%s\n",token);
         if (token!=NULL)
         {
             while (token!=NULL)
             {
-                valores_leidos[i]=atof(token);
+                valores_leidos[i]=atof(token);//Conversión de cadena a flotante
                 token=strtok(NULL,",");
                 i++;
             }
@@ -73,6 +86,7 @@ int main(){
         }
         
         i=0;
+        //Asignación de valores a cada uno de los operandos de la expresión postfija
         for(NODO* temporal = expresion_postfija->head;temporal!=NULL;temporal=temporal->sig){
             if(temporal->operador==false) {
                 temporal->valor=valores_leidos[i];
@@ -81,6 +95,7 @@ int main(){
         }
     }
 
+    //Impresión de los valores leídos
     printf("Valores leidos: \n");
     for(NODO* temporal = expresion_postfija->head;temporal!=NULL;temporal=temporal->sig){
             if(temporal->operador==false) {
@@ -97,8 +112,9 @@ int main(){
         getchar();
     }
 
+    //Guardado de los valores en el documento valores.txt
     if(opcion_01==1){
-        archivo=fopen("postfijas.txt","a+t");
+        archivo=fopen("valores.txt","a+t");
         for(NODO* temporal = expresion_postfija->head;temporal!=NULL;temporal=temporal->sig){
             if(temporal->operador==false) {
                 fprintf(archivo,"%c %f\n",temporal->info,temporal->valor);
@@ -106,12 +122,12 @@ int main(){
         }
     printf("\n");
         fclose(archivo);
-        printf("Los valores han sido guardados en el archivo postfijas.txt\n");
+        printf("Los valores han sido guardados en el archivo valores.txt\n");
     }
 
     printf("Expresion a evaluar: ");
     imprimir_cola(expresion_postfija);
-    evaluar(expresion_postfija);
+    evaluar(expresion_postfija);//Evaluación de la expresión postfija
     getchar();
     getchar(); 
     return EXIT_SUCCESS;
