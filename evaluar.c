776f7140996feda6ee365e96aa69881476c218cd
Barraganri <80717543+Barraganri@users.noreply.github.com>
2021-08-19 12:51:43 -0600

    Update evaluar.c
    Se agregaron comentarios

--- evaluar.c
+++ evaluar.c
@@ -4,22 +4,25 @@ void evaluar(COLA* expresion_postfija){
     int opcion_01;
     char info_placeholder='u',info_nodo_pila,caso;
     double a,b,c,valor_pila;
-    FILE* archivo;
+    FILE* archivo; //Apuntador usado para escrbir/leer un archivo
     PILA* p1=crear_pila();
+    //Se crea un ciclo for que vaya de nodo en nodo.
     for(NODO* temporal = expresion_postfija->head;temporal!=NULL;temporal=temporal->sig){
-            
+            //Si es un operando, este solo se apila
             if(temporal->operador==false){
-                info_nodo_pila=temporal->info;
-                valor_pila=temporal->valor;
-                push(p1,info_nodo_pila);
-                p1->head->valor=valor_pila;
+                info_nodo_pila=temporal->info; 
+                valor_pila=temporal->valor; 
+                push(p1,info_nodo_pila); //Se apila el operadoe
+                p1->head->valor=valor_pila; //Se le asigan el valor
             }
+            //Si se trata de un operador, se  sacan los 2 operandos anteriores y se operan.
             else{
                 b=p1->head->valor;
                 pop(p1);
                 a=p1->head->valor;
                 pop(p1);
                 caso=temporal->info;
+                //Switch que separa los casos dependiendo del operador.
                 switch(caso){
                     case('+'):
                         c=a+b;
@@ -54,20 +57,21 @@ void evaluar(COLA* expresion_postfija){
                 }
             }
         }
-
+    //Impresion de resultados
     printf("El resultado final es: %f\n",p1->head->valor);
+    //Impresion en el archivo
     printf("Desea guardar el resultado en un documento de texto?\n1)Si\n2)No\n");
     scanf("%d",&opcion_01);
-
+    //Ciclo while para evitar valores incorrectos
     while(opcion_01!=1 && opcion_01!=2){
         printf("Por favor elige una opcion valida\n");
         scanf("%d",&opcion_01);
         getchar();
     }
-
+    //Impresion de resultado en el archivo
     if(opcion_01==1){
         archivo=fopen("postfijas.txt","a+t");
-            fprintf(archivo,"El resultado es: %f",p1->head->valor);
+        fprintf(archivo,"El resultado es: %f",p1->head->valor);
         fclose(archivo);
         printf("El resultado ha sido guardado en el archivo postfijas.txt\n");
     }
